d2c0f31c7b1baae20f9e9d61685ad500fef1e1a9
Swaraj Panmand <swarajpanmand@gmail.com>
2024-04-08 18:31:22 +0530

    adding mongoose

---
+++ 03-task-manager/start/app.js
@@ -0,0 +1,29 @@
+
+const connectDB = require('./db/connect')
+const express = require('express')
+const app = express()
+const tasks = require('./route/tasks')
+require('dotenv').config()
+
+//middleware
+app.use(express.json())
+
+//routes
+app.get('/hello',(req,res)=>{
+    res.send('Task Manager')
+})
+app.use('/api/v1/tasks', tasks)
+
+
+const port = 3000
+const start = async ()=>{
+    try{ 
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}!`))
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+start()
