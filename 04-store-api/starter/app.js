37e4eb997493d41ebb96b48e5acf0b0671588271
Swaraj Panmand <swarajpanmand@gmail.com>
2024-04-11 21:09:21 +0530

    removing vscode

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,42 @@
-console.log('04 Store API')
+require('dotenv').config();
+// aync error
+
+
+const express = require('express');
+const app = express()
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+//middleware
+app.use(express.json())
+
+
+//routes
+app.get('/', (req,res)=>{
+    res.send('<h>Store API</h><a href="/api/v1/products">products route</a>')
+})
+
+app.use('/api/v1/products', productsRouter)
+
+//products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const PORT = process.env.PORT || 3000
+
+const start = async () =>{
+    try {
+        //db
+        await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => console.log(`Server is listening port ${PORT}...`));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
