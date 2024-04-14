b7cbe0f9956138f20f6384bde40673d5fe7bf568
Swaraj Panmand <swarajpanmand@gmail.com>
2024-04-14 17:04:43 +0530

    adding files

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-// aync error
+require('express-async-errors');
 
 
 const express = require('express');
--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,11 +1,35 @@
-
+const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res) =>{
-    res.status(200).json({msg:'products testing routes'})
+    const products = await Product.find({}).select('name price')
+    //console.log('Products createdAt:', products.map(product => product.createdAt));
+    res.status(200).json({products, nbHits:products.length})
 }
 
 const getAllProducts = async (req,res) =>{
-    res.status(200).json({msg:'products routes'})
+    const {featured, company, name, sort}  =req.query
+    const queryObject = {}
+
+    if(featured){
+        queryObject.featured = featured === 'true' ? true :false 
+    }
+    if(company){
+        queryObject.company =company
+    }
+    if(name){
+        queryObject.name = {$regex:name , $options:'i'}
+    }
+
+    let result = Product.find(queryObject)
+    if(sort){
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    else{
+        result = result.sort('createdAt')
+    }
+    const products = await result
+    res.status(200).json({products, nbHits:products.length})
 }
 
 module.exports = {
