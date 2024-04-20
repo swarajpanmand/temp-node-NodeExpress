c36cb4598a315491cd7c8ff0c965a21d6802ea2d
Swaraj Panmand <swarajpanmand@gmail.com>
2024-04-20 17:47:31 +0530

    almost completed jwt

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,13 +1,30 @@
 
-
+const {BadRequestError} = require('../errors')
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
 
 const login = async (req,res) =>{
-    res.send("Fake Loign/Register route")
+    const {username, password} =req.body
+
+    if(!username || !password){
+        throw new BadRequestError('Please provide password and username!!')
+    }
+
+    //just for the demo , normally provided by DB
+    const id = new Date().getDate
+ 
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+      })
+    res.status(200).json({msg:'user created',token})
 }
 
 const dashboard = async (req,res)=> {
+    console.log(req.user )
     const LuckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:`hello swaraj`, secret:`here is your authorized data ${LuckyNumber}`})
+    res.status(200).json({msg:`hello ${req.user.username}`, secret:`here is your authorized data ${LuckyNumber}`}) 
+    
+    
 }
 
 
