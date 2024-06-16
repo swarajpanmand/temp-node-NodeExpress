ca65be41651f9fce9513f613d902d51e323a77f7
Swaraj Panmand <swarajpanmand@gmail.com>
2024-06-16 19:47:14 +0530

    continuing course

---
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -0,0 +1,27 @@
+const getAllJobs = async(req,res)=>{
+    res.send('register route')
+}
+
+const getJob= async(req,res)=>{
+    res.send('register route')
+}
+
+const createJob = async(req,res)=>{
+    res.send('register route')
+}
+
+const updateJob = async(req,res)=>{
+    res.send('register route')
+}
+
+const deleteJob = async(req,res)=>{
+    res.send('register route')
+}
+
+module.exports = {
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob
+}
